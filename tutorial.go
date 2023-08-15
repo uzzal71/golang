0fbbf64fcf11a27286d347c66e27db6b7abceed8
uzzal71 <uzzalroy.acm@gmail.com>
2023-08-15 14:57:30 +0600

    Complete Scanner

--- tutorial.go
+++ tutorial.go
@@ -4,13 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	// "strconv"
+	"strconv"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Printf("Type the year you ware born: ")
 	scanner.Scan()
-	input := scanner.Text()
-	fmt.Printf("You typed: %q", input)
+	input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	fmt.Printf("You will be %d year old at the end of 2024", 2024 - input)
 }
