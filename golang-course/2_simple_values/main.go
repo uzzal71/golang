504324bb7909b2b1f874669d43eaa6a8db9b4c91
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-21 17:47:34 +0600

    simple values

---
+++ golang-course/2_simple_values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple value
+	// int
+	fmt.Println(1 + 2);
+	//string
+	fmt.Println("Hello world!")
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+	// floats
+	fmt.Println(10.78)
+	fmt.Println(7.3  / 2.5)
+}
