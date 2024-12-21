53bce42c00e6e47e62a2274429e69aad8a1485c9
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-21 17:59:52 +0600

    variables

---
+++ golang-course/3_variables/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "golan"
+	fmt.Println(name)
+
+	var company = "infosys";
+	fmt.Println(company)
+
+	var isAdult = true
+	fmt.Println(isAdult)
+
+	var age int32 = 30
+	fmt.Println(age)
+
+	// shorthand syntax
+	city := "Dhaka"
+	fmt.Println(city)
+
+	var country string
+	country = "Bangladesh"
+	fmt.Println(country)
+
+	var price float32
+	price = 50.6
+	fmt.Println(price)
+
+	var percentage = 5.8
+	fmt.Println(percentage)
+
+	discount := 10.2
+	fmt.Println(discount)
+}
