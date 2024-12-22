f6acc2837c4570a977f44616711c43921d1bdc0d
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-22 19:06:55 +0600

    pointers

---
+++ golang-course/14_closures/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func counter() func() int {
+	var count int = 0
+
+	return func() int {
+		count += 1
+		return count
+	}
+}
+
+func main() {
+	increment := counter()
+	fmt.Println(increment());
+}
