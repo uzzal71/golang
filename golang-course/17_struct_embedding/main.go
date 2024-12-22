3d5d0665fced7fe43c91ad3f180fc10881d9b36e
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-22 20:12:44 +0600

    structs embedding

--- golang-course/17_struct_embedding/main.go
+++ golang-course/17_struct_embedding/main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+// customer struct
+type customer struct {
+    name string
+    phone string
+}
+
+// conposition
+type order struct {
+	id string
+	amount float32
+	status string
+	createdAt time.Time
+    customer
+}
+
+
 func main() {
-	
+    // newCustomer := customer{
+    //     name: "Uzzal roy",
+    //     phone: "+8801314578964",
+    // }
+
+	newOrder := order{
+        id: "1",
+        amount: 25.4,
+        status: "received",
+        createdAt: time.Now(),
+        customer: customer{
+            name: "Uzzal roy",
+            phone: "+8801314578964",
+        },
+    }
+
+    newOrder.customer.name = "Tapas Roy"
+
+    fmt.Println(newOrder)
+
 }
