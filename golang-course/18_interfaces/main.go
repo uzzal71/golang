d43609b6180e0f5085ad8ab7e19dfce55712b619
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-23 23:42:05 +0600

    interfaces

--- golang-course/18_interfaces/main.go
+++ golang-course/18_interfaces/main.go
@@ -2,22 +2,27 @@ package main
 
 import "fmt"
 
-type payment struct {
-
-}
+type payment struct {}
 
 func (p payment) makePayment(amount float32) {
     razorpayPaymentGw := razorpay{}
 	razorpayPaymentGw.pay(amount)
+
+	stripePaymentGw := stripe{}
+	stripePaymentGw.pay(amount)
 }
 
-type razorpay struct {
-
-}
+type razorpay struct {}
 
 func (r razorpay) pay(amount float32) {
 	// logic to make payment
-	fmt.Println("Making payment using razorpay", amount)
+	fmt.Println("making payment using razorpay", amount)
+}
+
+type stripe struct {}
+
+func (s stripe) pay(amount float32) {
+	fmt.Println("making payment using stripe", amount)
 }
 
 func main() {
