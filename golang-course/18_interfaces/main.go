4058e743845079a6de7660398897ff6a229a1175
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-23 23:37:44 +0600

    interfaces

--- golang-course/18_interfaces/main.go
+++ golang-course/18_interfaces/main.go
@@ -1,11 +1,23 @@
 package main
 
+import "fmt"
+
 type payment struct {
 
 }
 
 func (p payment) makePayment(amount float32) {
-    
+    razorpayPaymentGw := razorpay{}
+	razorpayPaymentGw.pay(amount)
+}
+
+type razorpay struct {
+
+}
+
+func (r razorpay) pay(amount float32) {
+	// logic to make payment
+	fmt.Println("Making payment using razorpay", amount)
 }
 
 func main() {
