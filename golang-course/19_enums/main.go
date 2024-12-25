cfbd6c33582607cfd02fa111fedfe94da356dd07
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-25 20:00:20 +0600

    enums

--- golang-course/19_enums/main.go
+++ golang-course/19_enums/main.go
@@ -3,18 +3,19 @@ package main
 import "fmt"
 
 // enumerated types
-type OrderStatus int
+type OrderStatus string
+
 const (
-    Received OrderStatus = iota
-    Confirmed
-    Prepared
-    Delivered
+	Received  OrderStatus = "received" //iota auto increments default: 0
+	Confirmed             = "confirmed"
+	Prepared              = "prepared"
+	Delivered             = "delivered"
 )
 
 func changeOrderStatus(status OrderStatus) {
-    fmt.Println("changing order status to", status)
+	fmt.Println("changing order status to", status)
 }
 
 func main() {
-	changeOrderStatus(Received)
+	changeOrderStatus(Prepared)
 }
