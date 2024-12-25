52b99d037f375d83627dd4308e223a1929d74116
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-25 20:07:15 +0600

    generics

--- golang-course/20_generics/main.go
+++ golang-course/20_generics/main.go
@@ -2,21 +2,38 @@ package main
 
 import "fmt"
 
+/*
 func printSlice(items []int) {
 	for _, item := range items {
 		fmt.Println(item)
 	}
 }
 
+
 func printStringSlice(items []string) {
+	for _, item := range items {
+		fmt.Println(item)
+	}
+}
+*/
+
+func printSlice[T any](items []T) {
 	for _, item := range items {
 		fmt.Println(item)
 	}
 }
 
 func main() {
+	/*
+		nums := []int{1, 2, 3}
+		printSlice(nums)
+		names := []string{"golan", "typescript"}
+		printStringSlice(names)
+	*/
+
 	nums := []int{1, 2, 3}
-	names := []string{"golan", "typescript"}
 	printSlice(nums)
-	printStringSlice(names)
+	names := []string{"golan", "typescript"}
+	printSlice(names)
+
 }
