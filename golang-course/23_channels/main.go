ee540526d62822e3b53818df3d9c445a82a9126b
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-26 23:46:53 +0600

    channels

--- golang-course/23_channels/main.go
+++ golang-course/23_channels/main.go
@@ -2,11 +2,21 @@ package main
 
 import "fmt"
 
+func processNum(numChan chan int) {
+	fmt.Println("processing number", <-numChan)
+}
+
 func main() {
-	messageChan := make(chan string)
+	/*
+		messageChan := make(chan string)
 
-	messageChan <- "ping"
+		messageChan <- "ping"
 
-	msg := <-messageChan
-	fmt.Println(msg)
+		msg := <-messageChan
+		fmt.Println(msg)
+	*/
+
+	numChan := make(chan int)
+	go processNum(numChan)
+	numChan <- 5
 }
