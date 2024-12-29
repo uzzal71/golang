b32bf07626479bd659a5ee5efb0672f9b054dc8f
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-29 19:54:26 +0600

    student packages

--- students-api/cmd/students-api/main.go
+++ students-api/cmd/students-api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/uzzal71/students-api/internal/config"
+	"github.com/uzzal71/students-api/internal/http/handlers/student"
 )
 
 func main() {
@@ -21,9 +22,7 @@ func main() {
 	// setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome to students api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 
 	// setup server
 	server := http.Server{
