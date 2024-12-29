e111315328340ed7d0077e733264bff8cea709c3
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-29 17:51:31 +0600

    Packages

--- golang-course/26_packages/auth/credentials.go
+++ golang-course/26_packages/auth/credentials.go
@@ -2,6 +2,6 @@ package auth
 
 import "fmt"
 
-func loginWithCredentials(username string, password string) {
+func LoginWithCredentials(username string, password string) {
 	fmt.Println("Login user using", username, password)
 }
--- golang-course/26_packages/main.go
+++ golang-course/26_packages/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/uzzal71/podcast/auth"
+	"github.com/uzzal71/podcast/user"
+)
+
+func main() {
+	auth.LoginWithCredentials("uzzal@gmail.com", "123456")
+	session := auth.GetSession()
+	fmt.Println(session)
+
+	user := user.User{
+		Email: "sujon@gmail.com",
+		Name:  "Sujon Roy",
+	}
+
+	fmt.Println(user.Email, user.Name)
 }
