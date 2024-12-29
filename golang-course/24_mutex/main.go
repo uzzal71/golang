8c4fbe8ef60974f6bd5e572dd98ad170545e2ee8
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-29 15:01:14 +0600

    mutex

--- golang-course/24_mutex/main.go
+++ golang-course/24_mutex/main.go
@@ -1,18 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type post struct {
 	views int
 }
 
-func (p *post) inc() {
+func (p *post) inc(wg *sync.WaitGroup) {
+	defer wg.Done()
 	p.views += 1
 }
 
 func main() {
-	myPost := post{views: 0}
-	myPost.inc()
+	var wg sync.WaitGroup
 
+	myPost := post{views: 0}
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go myPost.inc(&wg)
+	}
+
+	wg.Wait()
 	fmt.Println(myPost.views)
 }
