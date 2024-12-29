0795771fa3495570c696a5d33acdf379ff4ba3d7
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-29 16:11:53 +0600

    Files

---
+++ golang-course/25_files/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("example.txt")
+	if err != nil {
+		// log the error
+		panic(err)
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		// log the error
+		panic(err)
+	}
+
+	fmt.Println("file name:", fileInfo.Name())
+	fmt.Println("file or folder:", fileInfo.IsDir())
+	fmt.Println("file size:", fileInfo.Size())
+}
