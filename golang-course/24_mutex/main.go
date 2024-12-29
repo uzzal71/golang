e589873445d0bc1e40df124e0e81778830a267ae
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-29 14:57:19 +0600

    mutex

--- golang-course/24_mutex/main.go
+++ golang-course/24_mutex/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+type post struct {
+	views int
+}
+
+func (p *post) inc() {
+	p.views += 1
+}
+
 func main() {
-	fmt.Println("Mutex")
+	myPost := post{views: 0}
+	myPost.inc()
+
+	fmt.Println(myPost.views)
 }
