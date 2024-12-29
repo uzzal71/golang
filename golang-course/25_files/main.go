e421767f9059d3388ac8cab4952b4aa5f9dc2310
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-29 16:12:43 +0600

    Files

--- golang-course/25_files/main.go
+++ golang-course/25_files/main.go
@@ -21,4 +21,6 @@ func main() {
 	fmt.Println("file name:", fileInfo.Name())
 	fmt.Println("file or folder:", fileInfo.IsDir())
 	fmt.Println("file size:", fileInfo.Size())
+	fmt.Println("file permission:", fileInfo.Mode())
+	fmt.Println("file modified at:", fileInfo.ModTime())
 }
