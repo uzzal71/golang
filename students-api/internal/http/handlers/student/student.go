9c2717ef3c6ad9337b2095ce1bda70232ed54785
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-30 23:03:29 +0600

    student create

--- students-api/internal/http/handlers/student/student.go
+++ students-api/internal/http/handlers/student/student.go
@@ -1,9 +1,13 @@
 package student
 
-import "net/http"
+import (
+	"log/slog"
+	"net/http"
+)
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Creating a student")
 		w.Write([]byte("welcome to students api"))
 	}
 }
