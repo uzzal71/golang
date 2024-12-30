1259837014a2f18e6735499b8e0f7a902f9182cc
uzzal71 <uzzalroy.acm@gmail.com>
2024-12-30 23:43:28 +0600

    student create

--- students-api/internal/http/handlers/student/student.go
+++ students-api/internal/http/handlers/student/student.go
@@ -1,12 +1,23 @@
 package student
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
 	"log/slog"
 	"net/http"
+
+	"github.com/uzzal71/students-api/internal/types"
 )
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var student types.Student
+		err := json.NewDecoder(r.Body).Decode(&student)
+		if errors.Is(err, io.EOF) {
+
+		}
+
 		slog.Info("Creating a student")
 		w.Write([]byte("welcome to students api"))
 	}
---
+++ students-api/internal/utils/response/response.go
@@ -0,0 +1,7 @@
+package response
+
+import "net/http"
+
+func WriteJson(w http.ResponseWriter, status int, data interface{}) {
+
+}
